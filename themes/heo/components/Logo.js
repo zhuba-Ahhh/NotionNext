ad1b0243ecc1361a15afcfd71184f2e41d779fc5
zhuba-Ahhh <3477826311@qq.com>
2023-09-16 23:12:24 +0800

    feat: :sparkles: add some config

--- themes/heo/components/Logo.js
+++ themes/heo/components/Logo.js
@@ -12,7 +12,8 @@ const Logo = props => {
                 <LazyImage src={siteInfo?.icon} width={24} height={24} alt={BLOG.AUTHOR} className='mr-4 hidden md:block' />
                 <div id='logo-text' className='group rounded-2xl flex-none relative'>
                     <div className='group-hover:opacity-0 opacity-100 visible group-hover:invisible text-lg my-auto rounded dark:border-white duration-200'>
-                        {siteInfo?.title || BLOG.TITLE}
+                        {/* {siteInfo?.title || BLOG.TITLE} */}
+                        ZhuBa Blog
                     </div>
                     <div className='flex justify-center rounded-2xl group-hover:bg-indigo-600 w-full group-hover:opacity-100 opacity-0 invisible group-hover:visible absolute top-0 py-1 duration-200'>
                         <Home className={'w-6 h-6 stroke-white stroke-2 '}/>
